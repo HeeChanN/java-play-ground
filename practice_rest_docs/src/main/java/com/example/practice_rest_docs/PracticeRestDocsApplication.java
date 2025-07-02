db25b1fbed0724216c9dd367c85c687904cbf588
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-07-02 18:00:48 +0900

    [Rest Docs] gradle 빌드 스크립트 생성

---
+++ practice_rest_docs/src/main/java/com/example/practice_rest_docs/PracticeRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_rest_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRestDocsApplication.class, args);
+	}
+
+}
---
+++ practice_rest_docs/src/test/java/com/example/practice_rest_docs/PracticeRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_rest_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
