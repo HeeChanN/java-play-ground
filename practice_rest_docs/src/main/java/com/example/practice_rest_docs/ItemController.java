ad0e0df8807429c88b607f500cbcfa8f1f42b6ec
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-07-08 14:59:31 +0900

    [Rest Docs] restdocs-api-spec으로 redoc 사용하기

--- practice_rest_docs/src/main/java/com/example/practice_rest_docs/ItemController.java
+++ practice_rest_docs/src/main/java/com/example/practice_rest_docs/ItemController.java
@@ -1,7 +1,6 @@
 package com.example.practice_rest_docs;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
--- practice_rest_docs/src/test/java/com/example/practice_rest_docs/ItemDoc.java
+++ practice_rest_docs/src/test/java/com/example/practice_rest_docs/ItemDoc.java
@@ -1,38 +1,63 @@
 package com.example.practice_rest_docs;
 
-import org.hibernate.validator.internal.metadata.raw.ConstrainedField;
+import com.epages.restdocs.apispec.ConstrainedFields;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import org.springframework.restdocs.constraints.Constraint;
 import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.List;
+import java.util.Map;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static com.epages.restdocs.apispec.ResourceDocumentation.*;
 import static org.springframework.restdocs.snippet.Attributes.key;
 
 public class ItemDoc {
 
     public static RestDocumentationResultHandler createItem(){
-        ConstraintDescriptions cons = new ConstraintDescriptions(ItemCreateDto.class);
+        //adoc 용
+        //ConstraintDescriptions cons = new ConstraintDescriptions(ItemCreateDto.class);
+
+        //restdocs-api-spec 용
+        //ConstrainedFields f = new ConstrainedFields(ItemCreateDto.class);
+
 
         return document(
                 "items/create-item",
-                requestFields(
-                        fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름")
-                                .attributes(key("constraints")
-                                        .value(String.join(", ", cons.descriptionsForProperty("name")))),
-                        fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명"),
-                        fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
-                                .attributes(key("constraints").value(
-                                        String.join(", ", cons.descriptionsForProperty("price"))))
-                ),
-                responseFields(
-                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이템 id"),
-                        fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름"),
-                        fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명"),
-                        fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
+                resource(
+                        ResourceSnippetParameters.builder()
+                                .tag("Item")
+                                .summary("아이템 생성")
+                                .description("아이템을 생성한다.")
+                                .requestFields(
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름")
+                                                .attributes(key("validationConstraints")
+                                                        .value(List.of(new Constraint(
+                                                                "javax.validation.constraints.NotBlank",
+                                                                Map.of()
+                                                        ))))
+                                        ,
+                                        fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명").optional(),
+                                        fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
+                                                .attributes(key("validationConstraints")
+                                                        .value(List.of(new Constraint(
+                                                                "javax.validation.constraints.NotNull",
+                                                                        Map.of()),
+                                                                new Constraint("javax.validation.constraints.Min",
+                                                                        Map.of("value",0))
+                                                                )
+                                                        )
+                                                )
+                                ).
+                                responseFields(
+                                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이템 id"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름"),
+                                        fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명"),
+                                        fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
+                                ).build()
                 )
         );
     }
@@ -40,21 +65,29 @@ public class ItemDoc {
     public static RestDocumentationResultHandler getItem(){
         return document(
                 "items/get-item-by-id",
-                pathParameters(
-                        parameterWithName("id").description("아이템 id")),
-                responseFields(
-                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이템 id"),
-                        fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름"),
-                        fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명"),
-                        fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
-                )
-        );
+                resource(ResourceSnippetParameters.builder()
+                        .tag("Item")
+                        .summary("아이템 단일 조회")
+                        .description("아이템을 id로 조회합니다.").
+                        pathParameters(
+                                parameterWithName("id").description("아이템 id")).
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이템 id"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("아이템 이름"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).description("아이템 부가 설명"),
+                                fieldWithPath("price").type(JsonFieldType.NUMBER).description("아이템 가격")
+                        ).build()
+                ));
     }
 
     public static RestDocumentationResultHandler errorSnippet(String identifier) {
         return document(identifier,
-                responseFields(fieldWithPath("status").description("HTTP 상태 코드"),
-                        fieldWithPath("message").description("에러 메시지"))
-        );
+                resource(ResourceSnippetParameters.builder()
+                        .tag("Item")
+                        .summary("아이템 생성")
+                        .description("새 아이템을 등록합니다.").
+                        responseFields(fieldWithPath("status").description("HTTP 상태 코드"),
+                                fieldWithPath("message").description("에러 메시지")).build()
+                ));
     }
 }
